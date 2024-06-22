8611649a47d406904c5adc1b112ba7890e14521f
Mertkan Karaaslan <122673964+zarabeth-yumie@users.noreply.github.com>
2024-06-23 01:20:19 +0300

    laura les

--- submit_form.php
+++ submit_form.php
@@ -1,38 +1,43 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get form data
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
-    
-    // Set recipient email address
+    // Sanitize and validate input data
+    $name = htmlspecialchars(strip_tags(trim($_POST["name"])));
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $message = htmlspecialchars(strip_tags(trim($_POST["message"])));
+
+    // Validate email
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        header("Location: contact.html?error=invalid_email");
+        exit;
+    }
+
+    // Email address where the form will be sent
     $to = "mertkangtr@hotmail.com";
 
-    // Set email subject
+    // Subject of the email
     $subject = "Message from Contact Form";
 
-    // Compose the email message
+    // Email content
     $email_message = "Name: $name\n";
     $email_message .= "Email: $email\n";
     $email_message .= "Message:\n$message";
 
-    // Set headers
+    // Email headers
     $headers = "From: $name <$email>\r\n";
     $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=utf-8\r\n";
+    $headers .= "X-Mailer: PHP/" . phpversion();
 
-    // Send email
+    // Attempt to send the email
     if (mail($to, $subject, $email_message, $headers)) {
-        // If mail is sent successfully, redirect back to contact.html with success message
         header("Location: contact.html?success=1");
         exit;
     } else {
-        // If mail sending fails, redirect back to contact.html with error message
-        header("Location: contact.html?error=1");
+        header("Location: contact.html?error=sending_failed");
         exit;
     }
 } else {
-    // If the request method is not POST, redirect back to contact.html
+    // Redirect to contact form if accessed directly
     header("Location: contact.html");
     exit;
 }
-?>
